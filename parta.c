3d8f63000c8e6af4e4ff81d3a61e769bbdfdf01e
drummatwit <drumm@wit.edu>
2025-11-07 14:46:37 -0500

    completed A4 Part A

--- parta.c
+++ parta.c
@@ -1,8 +1,33 @@
 #include <stdbool.h>
 #include <stddef.h>
+#include <stdio.h>
+#include <ctype.h>
+#include <string.h>
 
 // TODO: Include more header files (read the manpages to find out more!)
 
 int main(int argc, const char* argv[]) {
+
+    // Check if user put in no arguments
+    if (argc == 1) {
+        printf("ERROR: No arguments\n");
+        return 1;
+    }
+
+    //Loop over all arguments
+    for (int i = 1; i < argc; i++) {
+        const char* arg = argv[i];
+
+        //convert each letter to uppercase
+        size_t len = strlen(arg);
+        for (size_t j = 0; j < len; j++) {
+            putchar(toupper(arg[j])); 
+        }
+        // Put commas between arguments except for the last one
+        if ( i < argc - 1) {
+            putchar(',');
+        }
+    }
+    putchar('\n');
     return 0;
 }
